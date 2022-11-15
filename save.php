a8c9d4b6e2a6d1eb8eb936b4102d8d8a892faa40
Anatolii Gorin <92159095+anatoliigorin@users.noreply.github.com>
2022-11-15 21:49:10 +0500

    Update save.php

--- save.php
+++ save.php
@@ -1,5 +1,10 @@
 <?php  
   require 'db.php';
-  include 'page.php';
   
+  $data = $_POST['data'];
+  $lgn = $_POST['login'];
+  $sql = "SELECT userIndex FROM data WHERE userLogin = '$lgn';";
+  $result = $db->query($sql)->fetch_array();
+  file_put_contents('data/' . $result[0] . '.txt', $data);
+  include 'page.php';
 ?>
