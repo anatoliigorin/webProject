c36ef9cc7aa070303d5b2c60e6b489a90be88c25
Anatolii Gorin <92159095+anatoliigorin@users.noreply.github.com>
2022-11-15 21:07:20 +0500

    Add files via upload

---
+++ signIn.php
@@ -0,0 +1,33 @@
+
+<?php
+require 'db.php';
+    if(!empty($_POST))
+    {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        $table_name = "data";
+        $sqlLogin ="SELECT userIndex FROM $table_name WHERE userLogin = '$login';";
+        $resultLogin = $db->query($sqlLogin);
+        $valueLogin = $resultLogin->fetch_array();
+        if(!empty($valueLogin))
+        {
+            $sqlPassword ="SELECT userIndex FROM $table_name WHERE userLogin = '$login' AND userPassword = '$password';";
+            $resultPassword = $db->query($sqlPassword);
+            $valuePassword = $resultPassword->fetch_array();
+            if (!empty($valuePassword))
+            {
+                include 'page.php';
+            }
+            else
+            {
+            include 'index.php';
+            echo ("Пароль неправильный!");
+            }
+        }
+        else
+            {
+            include 'index.php';
+            echo ("Такого пользователя не существует!");
+            }
+    }
+?>
