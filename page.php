14e71610a141cd03a189275d43f2d657a4030c82
Anatolii Gorin <92159095+anatoliigorin@users.noreply.github.com>
2022-11-15 21:48:46 +0500

    Update page.php

--- page.php
+++ page.php
@@ -6,15 +6,15 @@
         </p>
     </form>
     <form action="save.php" method="POST">
-    <p align="center"><textarea cols="200" maxlenght="10000" rows="50" name="data">
-        <?php
-        require 'db.php';
-        $lgn = $_POST['login'];
-        $sql = "SELECT userIndex FROM data WHERE userLogin = '$lgn';";
-        $result = $db->query($sql)->fetch_array();
-        $data = file_get_contents('data/' . $result[0] . '.txt');
-        echo $data;
-    ?></textarea></p>
-    <button type="submit">Сохранить</button>
+        <p><input type="text" name="login" value="<?php $lgn = $_POST['login']; echo $lgn; ?>" readonly /></p>
+        <p align="center"><textarea cols="200" maxlenght="10000" rows="50" name="data"><?php
+            require 'db.php';
+            $lgn = $_POST['login'];
+            $sql = "SELECT userIndex FROM data WHERE userLogin = '$lgn';";
+            $result = $db->query($sql)->fetch_array();
+            $data = htmlentities(file_get_contents('data/' . $result[0] . '.txt'));
+            echo $data;
+        ?></textarea></p>
+        <button type="submit">Сохранить</button>
     </form>
 </html>
