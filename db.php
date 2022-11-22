92de39a21897cd30157a96f39001c8e42cd552a2
Anatolii Gorin <92159095+anatoliigorin@users.noreply.github.com>
2022-11-22 19:43:53 +0500

    Update db.php

--- db.php
+++ db.php
@@ -9,13 +9,6 @@
 	$db = new mysqli($ADDRESS, $USER, $PASSWORD, $NAME);
     	$db->set_charset($charset);
     	$db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
-	unset($ADDRESS, $NAME, $USER, $PASSWORD, $charset);	
-  
+	unset($ADDRESS, $NAME, $USER, $PASSWORD, $charset);
+	
 ?>
-<html>
-<form action="index.html">
-<p>
-<button type="submit">Вернуться</button>
-</p>
-</form>
-</html>
