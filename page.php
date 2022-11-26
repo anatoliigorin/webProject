ef9f2ab74a524bdf98b996a76aff48192db25e7e
ABOBA <8chewbacca@gmail.com>
2022-11-26 13:20:11 +0500

    Сохранение через ajax запрос

--- page.php
+++ page.php
@@ -1,9 +1,10 @@
+<!DOCTYPE html>
 <html>
     <head> 
         <link rel="stylesheet" href="css/style2.css">
         <title>Блокнот</title> 
         <style>
-            .comment
+            .text-area
             {
                 border: 3px solid #000000;
                 color: #ffffff;
@@ -41,7 +42,7 @@
                 filter:hue-rotate(0);color:#FFF;
                 text-decoration:none
             }
-            .save_btn
+            .save-btn
             {
                 font-size: 24px;
                 border-radius: 48px;
@@ -65,29 +66,44 @@
                 display: none;
             }
         </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>        
     </head>
 
     <body>
         <form action="index.php">
             <p>
                 <button class="return-btn" type="submit">Вернуться</button>
-            </p>
-        </form>
-        <form action="save.php" method="POST">
-            <p><input type="text" class="to-hide" name="login" value="<?php $lgn = $_POST['login']; echo $lgn; ?>" readonly /></p>
-            <p align="left">
-                <textarea class="comment" cols="50" maxlenght="10000" rows="10" name="data">
-                    <?php
-                        require 'db.php';
-                        $lgn = $_POST['login'];
-                        $sql = "SELECT userIndex FROM data WHERE userLogin = '$lgn';";
-                        $result = $db->query($sql)->fetch_array();
-                        $data = htmlentities(file_get_contents('data/' . $result[0] . '.txt'));
-                        echo $data;
-                    ?>
-                </textarea>
-            </p>
-            <button class="save_btn" type="submit">Сохранить</button>
-        </form>
+            </p> 
+        </form>       
+        <p align="left">
+            <textarea class="text-area" cols="50" maxlenght="10000" rows="10" id="txtArea">
+                <?php
+                    require 'db.php';
+                    $lgn = $_POST['login'];
+                    $sql = "SELECT userIndex FROM data WHERE userLogin = '$lgn';";
+                    $result = $db->query($sql)->fetch_array();
+                    $data = htmlspecialchars(file_get_contents('data/' . $result[0] . '.txt'));                               
+                    echo $data;
+                ?>
+            </textarea>
+        </p>
+        <button id="saveButton" class="save-btn">Сохранить</button>    
+        <script>
+            var data = <?php echo json_encode($_POST) ?>;
+            document.getElementById("saveButton").addEventListener("click", function() {sendToSave(data["login"]);}, false);
+
+            function sendToSave(loginValue)
+            {   
+                var dataValue = document.getElementById("txtArea").value;
+                $.ajax({
+                    method: "POST",  
+                    url: "save.php",
+                    data: {
+                        data: dataValue,                        
+                        login: loginValue
+                    }                                       
+                })                    
+            }
+        </script>
     </body>
 </html>
--- save.php
+++ save.php
@@ -1,10 +1,9 @@
 <?php  
   require 'db.php';
   
-  $data = $_POST['data'];
-  $lgn = $_POST['login'];
+  $data = $_POST["data"];
+  $lgn = $_POST["login"];
   $sql = "SELECT userIndex FROM data WHERE userLogin = '$lgn';";
   $result = $db->query($sql)->fetch_array();
   file_put_contents('data/' . $result[0] . '.txt', $data);
-  include 'page.php';
 ?>
